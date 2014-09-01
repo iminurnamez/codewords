00f75996d193f20da830064250eab3b68d01bebb
iminurnamez <easinerf@gmail.com>
2014-09-01 17:05:14 -0400

    added question mark

--- data/components/words.py
+++ data/components/words.py
@@ -1,7 +1,7 @@
 from collections import OrderedDict, Counter
 
-CODE_WORDS = OrderedDict([("PYTHON", 240),
-                                             ("BDFL", 160),
+CODE_WORDS = OrderedDict([("PYTHON", 200),
+                                             ("BDFL", 142),
                                              ("SPAM", 160),
                                              ("EGGS", 140),
                                              ("SELF", 160),
@@ -9,10 +9,10 @@ CODE_WORDS = OrderedDict([("PYTHON", 240),
                                              ("ANTIGRAVITY", 500),
                                              ("MAGIC", 200),
                                              ("ZEN", 120),
-                                             ("FUN", 120)])
+                                             ("LIBRARY", 300)])
 
 WORDS = """BEAUTIFUL BUILTIN CLASSY CODE CONTEXT DEBUG DUCK DUNDER
                     DYNAMIC EXCEPT EXPLICIT FLAT FUNCTION FORGIVENESS GUIDO
                     IDIOM INTERPRETED METHOD NAMESPACE OBJECT PROGRAM PYGAME
                     PYTHONISTA READABLE REFERENCE SIMPLE SPARSE SYNTAX TRUTHY
-                    TRY VALUE""".split()
+                    TRY VALUE WHITESPACE""".split()
